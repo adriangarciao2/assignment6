0ca018a4b12a8abac8f5a45e5409464ff83c6104
adriangarciao <garcia.adriann08@gmail.com>
2025-11-10 14:23:17 -0600

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+//        mvn exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D
+//        exec.args="codegen"
+    }
+
+}
